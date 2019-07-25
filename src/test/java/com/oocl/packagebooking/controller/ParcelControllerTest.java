da757a9bb8f9f7555115413cc59a5b6c2b01599c
OtaGran <ujt2h8@gmail.com>
2019-07-25 10:42:00 +0800

    [Alexander][test : should_add_a_appointment_when_post]

--- src/test/java/com/oocl/packagebooking/controller/ParcelControllerTest.java
+++ src/test/java/com/oocl/packagebooking/controller/ParcelControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,8 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,5 +52,18 @@ public class ParcelControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(new Gson().toJson(parcelList, List.class)));
+    }
+
+    @Test
+    public void should_add_a_appointment_when_post() throws Exception {
+        Appointment Alex = new Appointment(00000001L, "0000000001", System.currentTimeMillis());
+        Gson gson = new Gson();
+        when(parcelRepository.save(any())).thenReturn(Alex);
+        mockMvc.perform(post("/parcels")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(Alex)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(new Gson().toJson(Alex)));
     }
 }
