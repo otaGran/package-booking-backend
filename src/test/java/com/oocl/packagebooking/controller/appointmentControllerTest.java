407ea7ac6edbe78feb0688fb25537b816dd412fa
OtaGran <ujt2h8@gmail.com>
2019-07-25 09:20:54 +0800

    [Alexander][test : should_return_all_appointments_when_get]

---
+++ src/test/java/com/oocl/packagebooking/controller/appointmentControllerTest.java
@@ -0,0 +1,18 @@
+package com.oocl.packagebooking.controller;
+
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class AppointmentControllerTest {
+    @Autowired
+    private AppointmentController appointmentController;
+
+    @Test
+    public void should_return_all_appointments_when_get(){
+
+    }
+
+
+}
