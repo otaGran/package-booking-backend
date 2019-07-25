a6ed722a636fdcbd236edc76d769818220e7cc45
OtaGran <ujt2h8@gmail.com>
2019-07-25 10:34:05 +0800

    [Alexander][feat : should_add_a_appointment_when_post]

--- src/main/java/com/oocl/packagebooking/controller/AppointmentController.java
+++ src/main/java/com/oocl/packagebooking/controller/AppointmentController.java
@@ -1,11 +1,10 @@
 package com.oocl.packagebooking.controller;
 
+import com.oocl.packagebooking.model.Appointment;
 import com.oocl.packagebooking.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:9000")
@@ -21,5 +20,11 @@ public class AppointmentController {
     public ResponseEntity getAppointment(){
 
         return ResponseEntity.ok(appointmentRepository.findAll());
+    }
+
+    @PostMapping("/appointments")
+    public ResponseEntity addAppointment(@RequestBody Appointment appointment){
+
+        return ResponseEntity.ok(appointmentRepository.save(appointment));
     }
 }
